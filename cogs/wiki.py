cd14f742e448571bfbdac5df1e41e9caea6a4fcd
Quantaxer <phudel@uoguelph.ca>
2019-05-21 17:28:42 -0400

    Added summary command for wikipedia, included error checking

--- cogs/wiki.py
+++ cogs/wiki.py
@@ -15,10 +15,29 @@ class WikiCommands(commands.Cog):
 
     @wiki.command()
     async def search(self, ctx, *, arg):
-        """Searches wikipedia for any related pages"""
+        """Searches Wikipedia for any related pages"""
         data = wikipedia.search(arg)
         await ctx.send("These are all the pages I found about " + arg + ":")
         msg = "\n".join(data)
+        await ctx.send(msg)
+
+    @wiki.command()
+    async def summary(self, ctx, *, arg):
+        """Provides a brief 5 sentence summary for a certain Wikipedia page"""
+        try:
+            msg = "**This is what I found:**\n\n" + wikipedia.summary(arg, sentences=5)
+        except wikipedia.exceptions.DisambiguationError as e:
+            # occurs when the result is a disambiguation page
+            size = len(e.options)
+            # only display 10 options if more exist
+            if size > 9:
+                msg = "**This is a disambiguation page. Here are some related pages instead.**\n\n" + '\n'.join(e.options[0:9]) + "\n*And " + str(size - 10) + " more*"
+            else:
+                msg = "**This is a disambiguation page. Here are some related pages instead.**\n\n" + '\n'.join(e.options)
+        except wikipedia.exceptions.PageError as e:
+            # occurs if the page doesn't exist
+            msg = e
+
         await ctx.send(msg)
 
 
